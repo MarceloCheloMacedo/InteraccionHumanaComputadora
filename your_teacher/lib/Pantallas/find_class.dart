5fc7cb9cb120fb4654eaff592f94f80f8ddc33ad
Facundo Martínez <martinezfacundo23@gmail.com>
2023-11-22 19:17:53 -0300

    merge

--- your_teacher/lib/Pantallas/find_class.dart
+++ your_teacher/lib/Pantallas/find_class.dart
@@ -32,18 +32,11 @@ class _MyFind_ClassState extends State<MyFind_Class> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       drawer: NavDrawerStudentTeacher(),
       appBar: AppBar(
         title: Text(''),
       ),
 
-=======
-            drawer: NavDrawerStudentTeacher(),
-      appBar: AppBar(
-        title: Text(''),
-      ),
->>>>>>> 15225da555cee81b808436c5d0dcb457a9269ad6
       backgroundColor: const Color.fromRGBO(247, 225, 180, 1),
       body: Center(
         child: Container(
